071472fa6d6ea5ee0a1cd613daeab6badc4f59a4
Franco Guerendiain <fguerendiain33@gmail.com>
2017-03-20 21:19:55 -0300

    array creado con constructor

--- index.php
+++ index.php
@@ -1,5 +1,37 @@
 <?php
 
-    echo("Hola Mundo");
+    //EJERCICIO GUIA Nº6
+
+    //crear un array de 5 numeros random
+
+    $arrayUno;
+    $arrayDos;
+    $arratTres;
+
+    // 1 - con constructor
+    $arrayUno = array(rand(),rand(),rand());
+    var_dump($arrayUno);
+
+    // 2 - entre corchetes por cada elemento
+
+    $arrayDos(
+        [rand()]
+        [rand()]
+        [rand()]
+        [rand()]
+    );
+    var_dump($arrayDos);
+
+    // 3 - .Push() para cada uno de los elementos
+
+
+    // mostrar el contenido de un array
+
+
+
+    /*<------------------------------------------------->
+        var_dump() muestra el contenido del array
+
+        */
 
 ?>
