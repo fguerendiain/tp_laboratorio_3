ea1ca3e2177158c16ec44cf5fbdd97912788de2e
Franco Guerendiain <fguerendiain33@gmail.com>
2017-03-20 22:03:59 -0300

    se agrego archivo con guia de ejercicios

--- index.php
+++ index.php
@@ -4,19 +4,19 @@
 
     //crear un array de 5 numeros random
 
-    $arratTres;
 
     // 1 - con constructor
+
     $arrayUno = array(rand(1,15),rand(1,15),rand(1,15),rand(1,15),rand(1,15));
 
     echo("Array Uno: ");
-    var_dump($arrayUno);
+    var_dump($arrayUno); //muestra el contenido del array
 
     echo("<br><br>");
 
     // 2 - entre corchetes por cada elemento
-    $arrayDos = array();
 
+    $arrayDos = array();
     for( $i=0 ; $i < 5 ; $i++)
     {
         $arrayDos[$i] = rand(11,20);
@@ -27,26 +27,21 @@
 
     echo("<br><br>");
 
-    // 3 - .Push() para cada uno de los elementos
+    // 3 - .array_push() para cada uno de los elementos
 
     $arrayTres = array();
     for( $i=0 ; $i < 5 ; $i++)
     {
-        $arrayTres.array_push($arrayTres,rand(21,30));
+        array_push($arrayTres,rand(21,30));
     };
-    
+
     echo("Array Tres: ");
     var_dump($arrayTres);
 
     echo("<br><br>");
 
-    // mostrar el contenido de un array
 
 
 
-    /*<------------------------------------------------->
-        var_dump() muestra el contenido del array
-
-        */
 
 ?>
