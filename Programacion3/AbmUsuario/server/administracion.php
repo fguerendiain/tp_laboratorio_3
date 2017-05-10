96fc1dde38ac7299205daa81dff1b983edf414cd
alumno <alumno>
2017-05-09 21:10:04 -0300

    Clase08 - Guia Bower + Ejemplos con Bootstrap validator

--- Programacion3/AbmUsuario/server/administracion.php
+++ Programacion3/AbmUsuario/server/administracion.php
@@ -8,23 +8,11 @@
     $reqDni = $_POST["dni"];
     $reqPath_foto = $_POST["path_foto"];
 
-
-//    $auxUser = new Usuario();
-//      var_dump(new Usuario(123,"Mariano","Pestillo","m",31917440,"foto.jpg"));
-//    $auxUser = new Usuario(123,"Mariano","Pestillo","m",31917440,"foto.jpg");
-
-//        if($auxUser)
-//        {
-//            var_dump($auxUser);
-//            echo("No el Objeto");
-//        }
-
     $auxUser = new Usuario($reqLegajo,$reqNombre,$reqApellido,$reqSexo,$reqDni,$reqPath_foto);
-    if(Usuario::GuardarEnArchivo($auxUser))
+//    if(Usuario::GuardarEnArchivo($auxUser))
     {
         echo("Se guardo el archivo<br><br>")
     }
-//    Usuario::GuardarUsuarioenBD($auxUser);
 
     echo("HOLA<br><br>");
 
