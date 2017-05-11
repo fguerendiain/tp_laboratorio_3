212448b0f9005c24177663f05f0f8b5e5a3332da
Franco Guerendiain <fguerendiain@gmail.com>
2017-05-11 18:01:44 -0300

    Se agrego Login a AbmUsuario

--- Programacion3/AbmUsuario/js/views.js
+++ Programacion3/AbmUsuario/js/views.js
@@ -5,15 +5,57 @@ $(document).ready(function()
     var bajaURL = "../AbmUsuario/views/baja.html";
     var modificacionURL = "../AbmUsuario/views/modificacion.html";
 
+    //por defecto al cargar la pagina muestra el modal con el login
+    $('#loginModal').modal({
+        show: true,
+        backdrop: false
+    });
+
+    //si encuentra informacion en el localstorage, la valida y de ser correcta omite el login
+    if(localStorage.getItem("user") && localStorage.getItem("passw"))
+    {
+//        alert("habia algo en el localstorage");
+        var user = localStorage.getItem("user");
+        var pass = localStorage.getItem("passw");
+
+        validarUsuario(user,pass);
+    }
+
+    //al precionar SingIn valida los datos ingresados
+    $("#btnSingIn").click(function(){   
+    
+//        alert("preciono boton SIngIN");
+        var user = $('#txtUsuario').val();
+        var pass = $('#txtPassword').val();
+
+//        alert(user + " " + pass);
+        localStorage.setItem("user",user);    
+        localStorage.setItem("passw",pass);    
+
+        validarUsuario(user,pass);
+
+    });
+
     $("#menuButtonAlta").click(function(){
-        $("#actualView").load(altaURL)
+        $("#actualView").load(altaURL);
         });
 
     $("#menuButtonBaja").click(function(){
-        $("#actualView").load(bajaURL)
+        $("#actualView").load(bajaURL);
         });
 
     $("#menuButtonModif").click(function(){
-        $("#actualView").load(modificacionURL)
+        $("#actualView").load(modificacionURL);
         });
+
+//valida los datos de loguin (al momento harcodeados)
+function validarUsuario(user, pass){
+//    alert("entro a validar");
+    if(user == "hola" && pass == "hola")
+    {
+        $('#loginModal').modal("hide");
+    }
+}
+
 });
+
